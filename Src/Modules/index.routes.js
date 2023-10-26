14593cc1a202ae1a6957412cf12bfa0dec008c58
AbullahUsama087 <abdullah.usama087@gmail.com>
2023-10-26 22:34:38 +0300

    The Modules Controllers and Routes

---
+++ Src/Modules/index.routes.js
@@ -0,0 +1,21 @@
+import categoryRouter from "./Categories/category.routes.js";
+import subCategoryRouter from "./SubCategories/subCategory.routes.js";
+import brandRouter from "./Brands/brand.routes.js";
+import productRouter from "./Products/product.routes.js";
+import couponRouter from "./Coupons/coupon.routes.js";
+import authRouter from "./Authentication/auth.routes.js";
+import cartRouter from "./Carts/cart.routes.js";
+import orderRouter from "./Orders/order.routes.js";
+import reviewRouter from "./Reviews/review.routes.js";
+
+export {
+  categoryRouter,
+  subCategoryRouter,
+  brandRouter,
+  productRouter,
+  couponRouter,
+  authRouter,
+  cartRouter,
+  orderRouter,
+  reviewRouter,
+};
